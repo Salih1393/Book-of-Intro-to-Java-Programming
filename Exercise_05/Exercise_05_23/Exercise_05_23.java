7a46ddd8f9d1ba5131ab1a246cbf3eb6b99862cd
Salih1393 <msalihkunduz@gmail.com>
2020-04-22 22:31:16 -0700

    Exercise_05_23 is done.

--- Exercise_05/Exercise_05_23/Exercise_05_23.java
+++ Exercise_05/Exercise_05_23/Exercise_05_23.java
@@ -4,6 +4,25 @@ public class Exercise_05_23 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Exercise_05_23");
+		double sum = 0;
+		double n=1;
+		
+		for(int i=0; i<50000; i++) {
+				
+				sum += (1/n); 
+			
+				n++;
+		}		
+		n--;
+		System.out.println("Left to rigth calculation: "+sum);
+		
+		for(int i=0; i<50000; i++) {
+			
+			sum += (1/n); 
+		
+			n--;
+	}		
+	
+		System.out.println("Right to left calculation: "+sum);
 	}
 }
